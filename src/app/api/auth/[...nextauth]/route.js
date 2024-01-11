9b856cdbbb94cef2508850d953ecc314802bfe37
AlexisBallo2 <alexispballo@gmail.com>
2024-01-11 09:27:58 -0500

    pivot to prisma

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,17 +1,24 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import { User } from "../../../../models/User";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongoConnect";
 
-mongoose.connect(process.env.MONGO_URL);
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+
+const prisma = new PrismaClient()
+
+// mongoose.connect(process.env.MONGO_URL);
 
 export const authOptions = {
   secret: process.env.SECRET,
-  adapter: MongoDBAdapter(clientPromise),
+  // adapter: MongoDBAdapter(clientPromise),
+	adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -31,7 +38,9 @@ export const authOptions = {
         const email = credentials?.email;
         const password = credentials?.password;
 
-        const user = await User.findOne({ email });
+        const user = await prisma.user.findUnique({ 
+					where: { email : email}
+				});
         const passwordOk =
           user && (await bcrypt.compare(password, user.password));
         if (passwordOk) {
--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,12 +1,16 @@
-import { User } from "../../../models/User";
-import mongoose from "mongoose";
+// import { User } from "../../../models/User";
+// import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import { PrismaClient } from "@prisma/client"
 
 export async function POST(req) {
   const body = await req.json();
-  mongoose.connect(
-    "mongodb+srv://midd-dash:midddash23@cluster0.usnrwxk.mongodb.net/midd-dash"
-  );
+
+	const prisma = new PrismaClient()
+  // mongoose.connect(
+  //   "mongodb+srv://midd-dash:midddash23@cluster0.usnrwxk.mongodb.net/midd-dash"
+  // );
+	
   const pass = body.password;
   if (!pass?.length || pass.length < 5) {
     new Error("password must be at least 5 characters");
@@ -15,6 +19,13 @@ export async function POST(req) {
   var salt = bcrypt.genSaltSync(10);
   body.password = bcrypt.hashSync(notHashedPassword, salt);
 
-  const createdUser = await User.create(body);
+	const createdUser = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		})
+
+	prisma.$disconnect()
   return Response.json(createdUser);
 }
