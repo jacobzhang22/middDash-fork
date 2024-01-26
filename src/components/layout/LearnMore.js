cf351c6f8b544a6fa69b0a590ba1446c18e7014c
Jacob Zhang <jaco8123@gmail.com>
2024-01-26 10:59:53 -0500

    update learnmore

--- src/components/layout/LearnMore.js
+++ src/components/layout/LearnMore.js
@@ -7,11 +7,11 @@ export default function LearnMore() {
       <SectionHeaders mainHeader="Get Started" />
       <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
         <p>
-          <strong>1. Sign Up:</strong> Quick account setup or Google login.
+          <strong>1. Sign Up:</strong> Create an account or login with Google.
         </p>
         <p>
-          <strong>2. Fill Out Info:</strong> Enter your contact information and
-          delivery location.
+          <strong>2. Complete Your Profile:</strong> Enter in contact
+          information and delivery details.
         </p>
         <p>
           <strong>3. Place Order:</strong> Browse through a wide selection of
@@ -26,12 +26,12 @@ export default function LearnMore() {
           straight to your dasher.
         </p>
         <p>
-          <strong>6. Enjoy Fast Delivery:</strong> Your meal delivered in under
-          15 minutes to your door.
+          <strong>6. Enjoy Fast Delivery:</strong> Your order delivered in under
+          15 minutes straight to your door.
         </p>
         <p>
           <strong>Need Help?:</strong> Our support team is ready to assist. Use
-          the <em>Report</em> button down below for any issues.
+          the Report button down below for any issues.
         </p>
       </div>
     </section>
