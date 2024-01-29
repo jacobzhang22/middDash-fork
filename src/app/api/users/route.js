94ee40e4a42c042176233865ae8cfbf5e332823e
AlexisBallo2 <alexispballo@gmail.com>
2024-01-29 16:06:44 -0500

    fix admin mobile

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -6,6 +6,7 @@ export async function GET() {
   const users = await prisma.user.findMany({
     select: {
       id: true,
+      email: true,
       isDasher: true,
       isAdmin: true,
       name: true,
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
 // import { useProfile } from "@/components/UseProfile";
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import UserTabs from '@/components/layout/UserTabs';
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import UserTabs from "@/components/layout/UserTabs";
 
 export default function UsersPage() {
   const [users, setUsers] = useState([]);
   // const { loading, data } = useProfile();
 
   useEffect(() => {
-    fetch('/api/users').then((response) => {
+    fetch("/api/users").then((response) => {
       response.json().then((data) => {
         setUsers(data.users);
       });
@@ -29,25 +29,32 @@ export default function UsersPage() {
     <section className="max-w-2xl mx-auto mt-8">
       <UserTabs isAdmin />
       <div className="mt-8">
-        {users?.length > 0
-          && users.map((user) => (
-            <div key={user.id} className="bg-gray-100 rounded-lg mb-2 p-1 px-4 flex items-center gap-4">
-              <div className="grid grid-cols-2 md:grid-cols-3 gap-4 grow">
-                <div className="text-gray-900">
-                  <div className="space-x-2 md:space-x-10">
-                    {!!user.name && <span className="whitespace-nowrap">{user.name}</span>}
-                    {!!user.isAdmin && <span className="bg-[#fff71c] rounded-lg p-2">ADMIN</span>}
-                    {!!user.isDasher && <span className="bg-[#5e6df2] rounded-lg p-2">DASHER</span>}
-                  </div>
-                  {!user.name && <span className="italic">No name</span>}
+        {users?.length > 0 &&
+          users.map((user) => (
+            <div
+              key={user.id}
+              className="bg-gray-100 rounded-lg mb-2 p-1 px-4 flex items-center gap-4"
+            >
+              <div className=" flex flex-col md:flex-row space-y-2 space-x-0 md:space-x-2 md:space-y-0 items-center ">
+                <span className="whitespace-nowrap">
+                  {user.name ? user.name : "No Name"}
+                </span>
+                <div>
+                  <span className="text-gray-500">{user.email}</span>
                 </div>
-                <span className="text-gray-500">{user.email}</span>
-              </div>
-              <div>
+                <div>
+                  {!!user.isAdmin && (
+                    <span className="bg-[#fff71c] rounded-lg p-2">ADMIN</span>
+                  )}
+                  {!!user.isDasher && (
+                    <span className="bg-[#5e6df2] rounded-lg p-2">DASHER</span>
+                  )}
+                </div>
                 <Link className="button" href={`/users/${user.id}`}>
                   Edit
                 </Link>
               </div>
+              <div />
             </div>
           ))}
       </div>
